e5dc19dcc0fcc37dbf4c3880d6a063b75544cd19
ParfenovPeter <terrylapard@gmail.com>
2023-08-18 15:06:17 +0300

    Implemented more general Q-Cumulant methods for vn{2}, vn{4}: with all p,r,q Q-vectors and for Q-vectors with weights

--- src/dataframe/correlation/CorrelationFunctions.hpp
+++ src/dataframe/correlation/CorrelationFunctions.hpp
@@ -50,32 +50,90 @@ inline auto ScalarProduct(unsigned int h_u, unsigned int h_Q) {
 inline auto c2(unsigned int h_u) {
   return [h_u](const Qn::QVector &u) {
     double ret = 0.;
-    auto m = u.sumweights();
-    if (m < 2.) {
-      ret = NAN;
-    } else {
       auto Q = u.DeNormal();
+      auto m = Q.sumweights();
+    if (m < 2.) {
+      ret = 0.;
+    } else {
       ret = (ScalarProduct(Q, Q, h_u) - m) / (m * (m - 1.));
     }
     return ret;
   };
 }
-
-inline auto d2(unsigned int h_u) {
-  return [h_u](const Qn::QVector &p, const Qn::QVector &r) {
+  
+// Weighted v2{2} method.
+// Input Q-vectors:
+//   Q11 = sum w^1 * un
+//   Q12 = sum w^2 * un
+// Q11 and Q12 are the same except weight
+inline auto wc2(unsigned int h_u) {
+  return [h_u](const Qn::QVector &q11, const Qn::QVector &q12) {
     double ret = 0.;
-    auto m = p.sumweights();
-    if (m < 2.) {
-      return ret = NAN;
+    auto Q11.DeNormal();
+    auto Q12.DeNormal();
+    auto S12 = Q12.sumweights();
+    auto S11 = Q11.sumweights();
+    auto S21 = S11 * S11;
+    auto denom = S21 - S12;
+    if (denom == 0.) {
+      ret = 0.;
+    } else {
+      ret = (ScalarProduct(Q11, Q11, h_u) - S12) / denom;
+    }
+    return ret;
+  };
+} 
+
+// v2{2} method
+// p = sum 1. * un - POI
+// r = sum 1. * un - RFP
+// q = sum 1. * un - both POI & RFP
+inline auto d2(unsigned int h_u) {
+  return [h_u](const Qn::QVector &p, const Qn::QVector &r, const Qn::QVector &q) {
+    double ret = 0.;
+    auto mp = p.sumweights();
+    auto mr = r.sumweights();
+    if (mp < 2. || mr < 2.) {
+      return ret = 0.;
     } else {
       auto P = p.DeNormal();
       auto R = r.DeNormal();
+      auto Q = q.DeNormal();
       std::complex<double> p0{P.sumweights(), 0};
       std::complex<double> r0{R.sumweights(), 0};
+      std::complex<double> q0{Q.sumweights(), 0};
       std::complex<double> p1{P.x(h_u), P.y(h_u)};
       std::complex<double> r1{R.x(h_u), R.y(h_u)};
-      return (p1 * std::conj(r1) - p0).real() / (p0 * r0 - p0).real();
+      ret = (p1 * std::conj(r1) - q0).real() / (p0 * r0 - q0).real();
     }
+    return ret;
+  };
+}
+
+// Weighted v2{2} method
+// Input Q-vectors:
+//   p = sum 1. * un - POI
+//   r = sum w^1 * un - RFP
+//   q = sum w^1 * un  - both RFP and POI
+inline auto wd2(unisgned int h_u) {
+  return [h_u](const Qn::QVector &p, const Qn::QVector &r, const Qn::QVector &q) {
+    double ret = 0.;
+    auto mp = p.sumweights();
+    auto mr = r.sumweights();
+    if (mp < 2. || mr < 2.) {
+      return 0.;
+    } else {
+      auto P = p.DeNormal();
+      auto R = r.DeNormal();
+      auto Q = q.DeNormal();
+      std::complex<double> p0{P.sumweights(), 0};
+      std::complex<double> r0{R.sumweights(), 0};
+      std::complex<double> q0{Q.sumweights(), 0};
+      std::complex<double> p1{P.x(h_u), P.y(h_u)};
+      std::complex<double> r1{R.x(h_u), R.y(h_u)};
+      ret = (p1 * std::conj(r1) - q0).real() / (p0 * r0 - q0).real();
+    }
+    return ret;
   };
 }
 
@@ -177,29 +235,87 @@ inline auto nd4() {
   };
 }
 
+// v2{4} method
+// p = sum 1. * un - POI
+// r = sum 1. * un - RFP
+// q = sum 1. * un - both POI & RFP
 inline auto d4(unsigned int h_u) {
-  return [h_u](const Qn::QVector &p, const Qn::QVector &r) {
+  return [h_u](const Qn::QVector &p, const Qn::QVector &r, const Qn::QVector &q) {
     const auto R = r.DeNormal();
     const auto RM = r.sumweights();
     const auto P = p.DeNormal();
     const auto PM = p.sumweights();
-    const auto Q = p.DeNormal();
-    const auto QM = p.sumweights();
-    if (PM < 4.) return std::nan("");
+    const auto Q = q.DeNormal();
+    const auto QM = q.sumweights();
+    if (PM < 4.) return 0.;
+    const auto p0 = std::complex<double>{PM, 0.};
     const auto r0 = std::complex<double>{RM, 0.};
     const auto q0 = std::complex<double>{QM, 0.};
     const auto p1 = std::complex<double>{P.x(h_u), P.y(h_u)};
     const auto r1 = std::complex<double>{R.x(h_u), R.y(h_u)};
     const auto r2 = std::complex<double>{R.x(2 * h_u), R.y(2 * h_u)};
-    const auto q1 = std::complex<double>{P.x(h_u), P.y(h_u)};
-    const auto q2 = std::complex<double>{P.x(2 * h_u), P.y(2 * h_u)};
+    const auto q1 = std::complex<double>{Q.x(h_u), Q.y(h_u)};
+    const auto q2 = std::complex<double>{Q.x(2 * h_u), Q.y(2 * h_u)};
 
     auto c4 = p1 * r1 * std::conj(r1) * std::conj(r1) -
               q2 * std::conj(r1) * std::conj(r1) - p1 * r1 * std::conj(r2) -
-              2. * r1 * q0 * std::conj(r1) - 2. * p1 * r0 * std::conj(r1) +
-              4. * q1 * std::conj(r1) + 2. * r0 * q0 + 2. * r1 * std::conj(q1) +
-              2. * p1 * std::conj(r1) + q2 * std::conj(r2) - 6. * q0;
-    return c4.real() / ((q0 * r0 - 3. * q0) * (r0 - 1.) * (r0 - 2.)).real();
+              2. * r0 * p1 * std::conj(r1) - 2. * q0 * r1 * std::conj(r1) +
+              7. * q1 * std::conj(r1) - r1 * std::conj(q1) + q2 * std::conj(r2) +
+              2. * p1 * std::conj(r1) + 2. * q0 * r0 - 6. * q0;
+      
+    return c4.real() / ((p0 * r0 - 3. * q0) * (r0 - 1.) * (r0 - 2.)).real();
+  };
+};
+
+// Weighted v2{4} method
+// p10 = sum 1. * un - POI
+// r11 = sum w^1 * un - RFP
+// r22 = sum w^2 * un - RFP (for Q2n)
+// r13 = sum w^3 * un - RFP
+// r14 = sum w^4 * un - RFP
+// q21 = sum w^1 * un - both POI & RFP (for q2n)
+// q12 = sum w^2 * un - both POI & RFP
+// q13 = sum w^3 * un - both POI & RFP
+inline auto wd4(unsigned int h_u) {
+  return [h_u](const Qn::QVector &p10, const Qn::QVector &q21, const Qn::QVector &q12, const Qn::QVector &q13,
+               const Qn::QVector &r11, const Qn::QVector &r22, const Qn::QVector &r13, const Qn::QVector &r14) {
+    const auto P10 = p10.DeNormal();
+    const auto R11 = r11.DeNormal();
+    const auto R22 = r22.DeNormal();
+    const auto R13 = r13.DeNormal();
+    const auto R14 = r14.DeNormal();
+    const auto Q21 = q21.DeNormal();
+    const auto Q12 = q12.DeNormal();
+    const auto Q13 = q13.DeNormal();
+    const auto S11 = std::complex<double>{R11.sumweights(), 0.};
+    const auto S12 = std::complex<double>{R12.sumweights(), 0.};
+    const auto S13 = std::complex<double>{R13.sumweights(), 0.};
+    const auto S14 = std::complex<double>{R14.sumweights(), 0.};
+    const auto S21 = S11 * S11;
+    const auto S31 = S11 * S21;
+    const auto mp  = std::complex<double>{P10.sumweights(), 0.};
+    const auto s11 = std::complex<double>{Q11.sumweights(), 0.};
+    const auto s12 = std::complex<double>{Q12.sumweights(), 0.};
+    const auto s13 = std::complex<double>{Q13.sumweights(), 0.};
+    const auto denom = mp*(S31 - 3.*S11*S12 + 2.*S13) - 3.*(s11*(S21-S12)+2.*(s13-s12*S11));
+    if (denom.real() == 0) return 0.;
+
+    const auto fp10 = std::complex<double>{P10.x(h_u), P10.y(h_u)};
+    const auto fq21 = std::complex<double>{Q21.x(h_u), Q21.y(h_u)};
+    const auto fq12 = std::complex<double>{Q12.x(h_u), Q12.y(h_u)};
+    const auto fq13 = std::complex<double>{Q13.x(h_u), Q13.y(h_u)};
+    const auto fr11 = std::complex<double>{R11.x(h_u), R11.y(h_u)};
+    const auto fr22 = std::complex<double>{R22.x(h_u), R22.y(h_u)};
+    const auto fr13 = std::complex<double>{R13.x(h_u), R13.y(h_u)};
+    const auto fr14 = std::complex<double>{R14.x(h_u), R14.y(h_u)};
+
+    auto c4 = fp10 * fr11 * std::conj(fr11) * std::conj(fr11) -
+              fq21 * std::conj(fr11) * std::conj(fr11) - fp10 * fr11 * std::conj(fr22) -
+              2. * S12 * fp10 * std::conj(fr11) - 2. * s11 * fr11 * std::conj(fr11) +
+              7. * fq12 * std::conj(fr11) - fr11 * std::conj(fq12) + fq21 * std::conj(fr22) +
+              2. * fp10 * std::conj(fr13) + 2. * s11 * S12 - 6. * s13;
+      
+    return c4.real() / denom.real();
   };
 };
 
@@ -209,7 +325,7 @@ inline auto c4(unsigned int h_u) {
     auto Q = u.DeNormal();
     auto M = u.sumweights();
     if (M < 4.) {
-      ret = NAN;
+      ret = 0.;
     } else {
       auto x = Q.x(h_u);
       auto y = Q.y(h_u);
@@ -224,6 +340,48 @@ inline auto c4(unsigned int h_u) {
       auto term_2 = (2 * (M - 2) * Q_mag * Q_mag - M * (M - 3)) /
                     (M * (M - 1) * (M - 2) * (M - 3));
       ret = term_1 - 2 * term_2;
+    }
+    return ret;
+  };
+}
+
+// Weighted v2{4} method.
+// Input Q-vectors:
+//   Q11 = sum w^1 * un
+//   Q22 = sum w^2 * un
+//   Q13 = sum w^3 * un
+//   Q14 = sum w^4 * un
+// Q11, Q22, Q13, Q14 are the same except weights
+inline auto wc4(unsigned int h_u) {
+  return [h_u](const Qn::QVector &Q11, const Qn::QVector &Q22, const Qn::QVector &Q13, const Qn::QVector &Q14) {
+    double ret = 0.;
+    auto fQ11 = Q11.DeNormal();
+    auto fQ22 = Q22.DeNormal();
+    auto fQ13 = Q11.DeNormal();
+    auto fQ14 = Q14.DeNormal();
+    auto Q11_mag = std::sqrt(fQ11.x(h_u)*fQ11.x(h_u) + fQ11.y(h_u)*fQ11.y(h_u));
+    auto Q11_mag2 = Q11_mag * Q11_mag;
+    auto Q11_mag4 = Q11_mag2 * Q11_mag2;
+    auto Q22_mag = std::sqrt(fQ22.x(2*h_u)*fQ22.x(2*h_u) + fQ22.y(2*h_u)*fQ22.y(2*h_u));
+    auto Q22_mag2 = Q22_mag * Q22_mag;
+    auto S11 = fQ11.sumweights();
+    auto S12 = fQ22.sumweights();
+    auto S13 = fQ13.sumweights();
+    auto S14 = fQ14.sumweights();
+    auto S21 = S11 * S11;
+    auto S41 = S21 * S21;
+    auto S22 = S12 * S12;
+    auto denom = S41 - 6.*S12*S21 + 8.*S13*S11 + 3.*S22 - 6.*S14;
+    if (denom == 0.) {
+      ret = 0.;
+    } else {
+      auto scalar_term = (Q11_mag4 + Q22_mag2 - 4.*S12*Q11_mag2) / denom;
+      auto re1 = 8. * ScalarProduct(fQ13, fQ11, h_u) / denom;
+      auto re2 = 2. * (fQ22.x(2*h_u)*fQ11.x(h_u)*fQ11.x(h_u) - fQ22.x(2*h_u)*fQ11.y(h_u)*fQ11.y(h_u) +
+                       fQ22.y(2*h_u)*fQ11.y(h_u)*fQ11.x(h_u) + fQ22.y(2*h_u)*fQ11.x(h_u)*fQ11.y(h_u)) / denom;
+      auto real_term = re1 - re2;
+      auto weight_term = (2.*S22 + 6.*S14) / denom;
+      ret = scalar_term + real_term - weight_term;
     }
     return ret;
   };
